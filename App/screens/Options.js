198058adea0edfb9f471be989efddfa2ed87ec15
Spencer Carli <spencercarli@gmail.com>
2020-03-23 10:52:51 -0500

    S3: Opening Links

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, Linking, Alert } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import colors from "../constants/colors";
 import { RowItem, RowSeparator } from "../components/RowItem";
+
+const openLink = url =>
+  Linking.openURL(url).catch(() =>
+    Alert.alert("Sorry, something went wrong.", "Please try again later.")
+  );
 
 export default () => {
   return (
@@ -21,7 +26,11 @@ export default () => {
 
         <RowItem
           title="React Native Basics"
-          onPress={() => alert("todo!")}
+          onPress={() =>
+            openLink(
+              "https://learn.handlebarlabs.com/p/react-native-basics-build-a-currency-converter"
+            )
+          }
           rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
         />
 
@@ -29,7 +38,7 @@ export default () => {
 
         <RowItem
           title="React Native by Example"
-          onPress={() => alert("todo!")}
+          onPress={() => openLink("https://reactnativebyexample.com")}
           rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
         />
       </ScrollView>
