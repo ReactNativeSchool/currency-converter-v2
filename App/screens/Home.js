9e5bd317627a571910abceb07d91ffb1480bebea
Spencer Carli <spencercarli@gmail.com>
2020-03-23 11:10:47 -0500

    S4: Working with Images

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, Dimensions, Image } from "react-native";
 
 import colors from "../constants/colors";
+
+const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.blue
+    backgroundColor: colors.blue,
+    justifyContent: "center"
+  },
+  logoContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20
+  },
+  logoBackground: {
+    width: screen.width / 0.45,
+    height: screen.width * 0.45
+  },
+  logo: {
+    position: "absolute",
+    width: screen.width * 0.25,
+    height: screen.width * 0.25
   }
 });
 
@@ -14,6 +31,18 @@ export default () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../assets/images/background.png")}
+          style={styles.logoBackground}
+          resizeMode="contain"
+        />
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   );
 };
