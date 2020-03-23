b0fc421da0403ca36fd3a243ab4cae3ce3cee930
Spencer Carli <spencercarli@gmail.com>
2020-03-23 10:51:22 -0500

    S3: Making scrollable area

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import colors from "../constants/colors";
@@ -7,30 +7,32 @@ import { RowItem, RowSeparator } from "../components/RowItem";
 
 export default () => {
   return (
-    <SafeAreaView>
-      <RowItem
-        title="Themes"
-        onPress={() => alert("todo!")}
-        rightIcon={
-          <Entypo name="chevron-right" size={20} color={colors.blue} />
-        }
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem
+          title="Themes"
+          onPress={() => alert("todo!")}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        title="React Native Basics"
-        onPress={() => alert("todo!")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          title="React Native Basics"
+          onPress={() => alert("todo!")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        title="React Native by Example"
-        onPress={() => alert("todo!")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          title="React Native by Example"
+          onPress={() => alert("todo!")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
