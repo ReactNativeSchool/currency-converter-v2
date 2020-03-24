5afce751ebb33c3858d40dbd9b335a563ef2caa4
Spencer Carli <spencercarli@gmail.com>
2020-03-24 11:00:10 -0500

    S6: Present Currency List as Moodal

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import Options from "../screens/Options";
 import CurrencyList from "../screens/CurrencyList";
+import colors from "../constants/colors";
 
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
@@ -15,18 +18,38 @@ const MainStackScreen = () => (
       options={{ headerShown: false }}
     />
     <MainStack.Screen name="Options" component={Options} />
-    <MainStack.Screen
+  </MainStack.Navigator>
+);
+
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator mode="modal">
+    <ModalStack.Screen
+      name="Main"
+      component={MainStackScreen}
+      options={{ headerShown: false }}
+    />
+    <ModalStack.Screen
       name="CurrencyList"
       component={CurrencyList}
-      options={({ route }) => ({
-        title: route.params.title
+      options={({ navigation, route }) => ({
+        title: route.params && route.params.title,
+        headerLeft: null,
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.pop()}
+            style={{ paddingHorizontal: 10 }}
+          >
+            <Entypo name="cross" size={30} color={colors.blue} />
+          </TouchableOpacity>
+        )
       })}
     />
-  </MainStack.Navigator>
+  </ModalStack.Navigator>
 );
 
 export default () => (
   <NavigationContainer>
-    <MainStackScreen />
+    <ModalStackScreen />
   </NavigationContainer>
 );
