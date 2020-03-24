3cf28652dbbe3f0b29dca9467c177bcc5f72ba75
Spencer Carli <spencercarli@gmail.com>
2020-03-24 09:32:12 -0500

    S4: Creating Button component

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -11,6 +11,7 @@ import { format } from "date-fns";
 
 import colors from "../constants/colors";
 import { ConversionInput } from "../components/ConversionInput";
+import { Button } from "../components/Button";
 
 const screen = Dimensions.get("window");
 
@@ -94,6 +95,7 @@ export default () => {
           "MMM do, yyyy"
         )}`}
       </Text>
+      <Button text="Reverse Currencies" onPress={() => swapCurrencies()} />
     </View>
   );
 };
