9c0c3827b85d3a8f686ed496157f8760b4bfee77
Spencer Carli <spencercarli@gmail.com>
2020-03-24 10:30:22 -0500

    S5: configure stack nav

---
+++ App/config/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../screens/Home";
+import Options from "../screens/Options";
+
+const MainStack = createStackNavigator();
+const MainStackScreen = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen name="Home" component={Home} />
+    <MainStack.Screen name="Options" component={Options} />
+  </MainStack.Navigator>
+);
+
+export default () => (
+  <NavigationContainer>
+    <MainStackScreen />
+  </NavigationContainer>
+);
--- App/index.js
+++ App/index.js
@@ -1,4 +1,4 @@
 import React from "react";
-import Home from "./screens/Home";
+import Navigation from "./config/Navigation";
 
-export default () => <Home />;
+export default () => <Navigation />;
