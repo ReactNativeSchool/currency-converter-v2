0a28ed9ab879cb8757a7935f73b3f722da31176b
Spencer Carli <spencercarli@gmail.com>
2020-03-25 10:17:26 -0500

    S8: Using API to get rates

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -65,15 +65,17 @@ const styles = StyleSheet.create({
 });
 
 export default ({ navigation }) => {
-  const { baseCurrency, quoteCurrency, swapCurrencies } = useContext(
-    ConversionContext
-  );
+  const {
+    baseCurrency,
+    quoteCurrency,
+    swapCurrencies,
+    date,
+    rates
+  } = useContext(ConversionContext);
   const [value, setValue] = useState("100");
-
-  const conversionRate = 0.89824;
-  const date = "2020-03-23";
-
   const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  const conversionRate = rates[quoteCurrency];
 
   return (
     <View style={styles.container}>
@@ -127,10 +129,8 @@ export default ({ navigation }) => {
             />
           </View>
           <Text style={styles.text}>
-            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(
-              new Date(date),
-              "MMM do, yyyy"
-            )}`}
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${date &&
+              format(new Date(date), "MMM do, yyyy")}`}
           </Text>
           <Button text="Reverse Currencies" onPress={() => swapCurrencies()} />
           <KeyboardSpacer onToggle={visible => setScrollEnabled(visible)} />
--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,10 +1,27 @@
 import React, { createContext, useState } from "react";
+import { Alert } from "react-native";
+
+import { api } from "./api";
 
 export const ConversionContext = createContext();
 
 export const ConversionContextProvider = ({ children }) => {
-  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [baseCurrency, _setBaseCurrency] = useState("USD");
   const [quoteCurrency, setQuoteCurrency] = useState("GBP");
+  const [date, setDate] = useState();
+  const [rates, setRates] = useState({});
+
+  const setBaseCurrency = currency => {
+    return api(`/latest?base=${currency}`)
+      .then(res => {
+        _setBaseCurrency(currency);
+        setDate(res.date);
+        setRates(res.rates);
+      })
+      .catch(error => {
+        Alert.alert("Sorry, something went wrong.", error.message);
+      });
+  };
 
   const swapCurrencies = () => {
     setBaseCurrency(quoteCurrency);
@@ -16,7 +33,9 @@ export const ConversionContextProvider = ({ children }) => {
     quoteCurrency,
     setBaseCurrency,
     setQuoteCurrency,
-    swapCurrencies
+    swapCurrencies,
+    date,
+    rates
   };
 
   return (
