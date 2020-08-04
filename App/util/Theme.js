5dc19c20fd803ab5124b5edaf2aee85f94ce67cc
Spencer Carli <spencercarli@gmail.com>
2020-08-04 11:00:08 -0500

    S11: Remember theme selection between sessions

--- App/util/Theme.js
+++ App/util/Theme.js
@@ -1,15 +1,31 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
 
 import colors from "../constants/colors";
 
 const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-  const [themeColor, setThemeColor] = useState(colors.blue);
+  const [themeColor, _setThemeColor] = useState(colors.blue);
+
+  const setThemeColor = (color) => {
+    _setThemeColor(color);
+    AsyncStorage.setItem("CurrencyConverter::THEME_COLOR", color);
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem("CurrencyConverter::THEME_COLOR").then((color) => {
+      if (color) {
+        setThemeColor(color);
+      } else {
+        setThemeColor(colors.blue);
+      }
+    });
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ themeColor, setThemeColor }}>
-      {children}
+      {themeColor ? children : null}
     </ThemeContext.Provider>
   );
 };
